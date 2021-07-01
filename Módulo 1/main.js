28dfd82cc147722a7f399880f9edcd5bd7638a7b
renatotnk <renato-tnk@live.com>
2021-06-30 23:56:59 -0300

    Praticando Flexbox e Media Queries..

--- Módulo 1/main.js
+++ Módulo 1/main.js
@@ -1,7 +1,8 @@
 //alert("Hello World");
 
 function atualizacao(){
-    var texto1 = document.getElementById("texto1");
-    texto1.innerText = "OMEGALUL";
-    texto1.style.color = "#00FF00";
+    var descricao = document.getElementById("descricao");
+    console.log(descricao)
+    descricao.innerText = "OMEGALUL";
+    descricao.style.color = "#00FF00";
 }
